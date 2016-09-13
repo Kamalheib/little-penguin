b58dc61de4e7fb1c721dfb3735fd723781b261f3
Kamal Heib <kamalheib1@gmail.com>
2016-09-13 18:58:28 +0300

    little-penguin: Task04
    Coding style fixes
    
    Signed-off-by: Kamal Heib <kamalheib1@gmail.com>

---
+++ Task04/main.c
@@ -0,0 +1,52 @@
+/*
+ * Copyright (c) 2016 Kamal Heib.  All rights reserved.
+ *
+ * This software is available to you under a choice of one of two
+ * licenses.  You may choose to be licensed under the terms of the GNU
+ * General Public License (GPL) Version 3, available from the file
+ * COPYING in the main directory of this source tree, or the
+ * OpenIB.org BSD license below:
+ *
+ *     Redistribution and use in source and binary forms, with or
+ *     without modification, are permitted provided that the following
+ *     conditions are met:
+ *
+ *      - Redistributions of source code must retain the above
+ *        copyright notice, this list of conditions and the following
+ *        disclaimer.
+ *
+ *      - Redistributions in binary form must reproduce the above
+ *        copyright notice, this list of conditions and the following
+ *        disclaimer in the documentation and/or other materials
+ *        provided with the distribution.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+ * BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+ * ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#include <linux/module.h>
+
+MODULE_AUTHOR("Kamal Heib <kamalheib1@gmail.com>");
+MODULE_DESCRIPTION("little penguin Task1");
+MODULE_VERSION("0.1");
+MODULE_LICENSE("GPL v3");
+
+static int __init t1_init(void)
+{
+	pr_debug("Hello World!");
+	return 0;
+}
+
+static void __exit t1_exit(void)
+{
+	pr_debug("Good bye!");
+}
+
+module_init(t1_init);
+module_exit(t1_exit);
